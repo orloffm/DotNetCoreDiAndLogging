1f8c8c0a3d634a2b1380226f0e52ec10318c7830
Mikhail Orlov <orloffm@gmail.com>
2018-04-29 12:08:20 +0200

    before web

---
+++ CoreWebApplication/Controllers/AnimalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApplication.Controllers
+{
+[Route("api/[controller]")]
+public class AnimalController : Controller
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "temp";
+    }
+}
+}
